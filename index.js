7ea5f77ab9df5b42d656f3fc01a4490b3b55fb0b
Ian Mwangi <ian.mwangi2@students.moringaschool.com>
2024-07-02 09:50:29 +0300

    modifications

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solution here
+  //findMatching
+function findMatching(drivers, name) {
+    return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
+}
+
+   //fuzzyMatch
+function fuzzyMatch (drivers, letter) {
+    letter = letter.toLowerCase();
+    return drivers.filter(driver => driver.toLowerCase().startsWith(letter));
+}
+
+  //matchName
+function matchName (drivers, name) {
+    return drivers.filter(driver => driver.name.toLowerCase() === name.toLowerCase());
+}
